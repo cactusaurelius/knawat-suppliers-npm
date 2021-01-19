5be1bd1ce0818536cb8f54a393ba82f0058648be
Abdelrahman <dev@ismail9k.com>
2021-01-19 13:57:21 +0300

    chore: support knawat env variables

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,7 +1,7 @@
 const config = {
   SUPPLIERS_API_URL : (process.env.NODE_ENV && process.env.NODE_ENV === 'production') ? 'https://suppliers.knawat.io/api' : 'https://dev.suppliers.knawat.io/api',
-  BASIC_USER : process.env.BASIC_USER,
-  BASIC_PASS : process.env.BASIC_PASS, 
+  BASIC_USER : process.env.KNAWAT_USER || process.env.BASIC_USER,
+  BASIC_PASS : process.env.KNAWAT_PASS || process.env.BASIC_PASS, 
   HEADERS : {
     'Content-Type': 'application/json'
   }
